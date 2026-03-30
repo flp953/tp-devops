9d39df4549813775c5f2033e3d1c1d024a1f87e0
flp953 <08filipelucas@gmail.com>
2026-03-30 13:38:15 +0200

    TP3 - variables d'environnement

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello B3 DevOps"
+@app.route("/")
+def home():
+    message = os.getenv("MESSAGE", "Hello Default")
+    return message
 
 app.run(host="0.0.0.0", port=5000)

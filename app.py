746ed09e73f5af8e2849efc3363ed106e21b59b7
flp953 <08filipelucas@gmail.com>
2026-03-31 13:43:19 +0200

    fix app.py version finale

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask
-import time
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    time.sleep(0.1)
-    return "Hello DevOps slow version"
+    return "Hello DevOps"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
